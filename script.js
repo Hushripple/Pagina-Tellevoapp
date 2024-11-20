1ae6f5d0c1fea5170badca8a774938c1142eb844
Hushripple <vaenuslazorbeamz@gmail.com>
2024-11-19 23:46:38 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+// Efecto de desplazamiento suave para todas las secciones
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
