0caffef38b5a01e5bedd57ad09a7977c7a483057
Hushripple <vaenuslazorbeamz@gmail.com>
2024-12-02 16:13:35 -0300

    Versión 2

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// Efecto de desplazamiento suave para todas las secciones
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-  
+const bg = [
+    'img/bg.png',
+    'img/bg1.jpg',
+    'img/bg2.png',
+    'img/bg3.jpg',
+    'img/bg4.jpg',
+    'img/bg5.png',
+    'img/bg6.png',
+
+];
+
+function cambiarBg() {
+    const randomBg = bg[Math.floor(Math.random() * bg.length)];
+
+    document.body.style.background = `url(${randomBg}) no-repeat center center fixed`;
+    document.body.style.backgroundSize = 'cover';  
+}
+
+window.onload = cambiarBg;
